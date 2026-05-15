f8a9d8e2e441ba577235adc257a0fa9a048fed71
valdemarbanganga13 <valdemarbanganga13@icloud.com>
2026-05-15 16:08:49 +0200

    Delete aerohub-connector directory

--- aerohub-connector/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  connectRadar: (userId) => ipcRenderer.send('connect-radar', userId),
-  disconnectRadar: () => ipcRenderer.send('disconnect-radar'),
-  onStatusUpdate: (callback) => ipcRenderer.on('status-update', (_event, type, message) => callback(type, message))
-});
